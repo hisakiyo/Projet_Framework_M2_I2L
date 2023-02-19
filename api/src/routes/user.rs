be55660c4885df78eb6d74ee2a29654386571e60
hisakiyo <hisakiyo.games@gmail.com>
2023-02-19 18:25:01 +0100

    Optimisation code Users

--- api/src/routes/user.rs
+++ api/src/routes/user.rs
@@ -1,18 +1,12 @@
-use crate::models::{NewUser, User, LoginUser};
-use rocket::{http::{Cookie, Cookies}, http::Status};
+use crate::{models::{NewUser, User, LoginUser},schema,DbConn,};
+use rocket::{http::{Cookie, Cookies},http::Status};
 use rocket_contrib::json::{Json, JsonValue};
-use diesel::{self, prelude::*};
-extern crate chrono;
+use diesel::prelude::*;
 use config::{Config, ConfigError, File};
-
-
-use crate::schema;
-use crate::DbConn;
 use bcrypt::{DEFAULT_COST, hash, verify};
 use jsonwebtoken::{encode, Header, EncodingKey};
+use chrono::{Utc, Duration};
 
-
-// struct claim
 #[derive(Debug, Serialize, Deserialize)]
 struct Claim {
     email: String,
@@ -31,9 +25,7 @@ fn get_jwt() -> Result<String, ConfigError> {
 
 #[get("/users")]
 pub fn get_users(conn: DbConn) -> Json<Vec<User>> {
-    use schema::users::dsl::*;
-
-    let results = users
+    let results = schema::users::table
         .limit(5)
         .load::<User>(&*conn)
         .expect("Error loading users");
@@ -41,16 +33,11 @@ pub fn get_users(conn: DbConn) -> Json<Vec<User>> {
     Json(results)
 }
 
-// Register a new user
-// Register a new user
 #[post("/register", format = "json", data = "<new_user>")]
 pub fn register(new_user: Json<NewUser>, conn: DbConn) -> Result<JsonValue, Status> {
-    use schema::users;
-
-    // Vérifier si l'email existe déjà
-    let email_exists = users::table
-        .filter(users::email.eq(&new_user.email))
-        .select(users::id)
+    let email_exists = schema::users::table
+        .filter(schema::users::email.eq(&new_user.email))
+        .select(schema::users::id)
         .first::<i32>(&*conn)
         .optional()
         .expect("Error loading user");
@@ -59,20 +46,18 @@ pub fn register(new_user: Json<NewUser>, conn: DbConn) -> Result<JsonValue, Stat
         return Err(Status::Conflict);
     }
 
-    // Hasher le mot de passe
     let hashed_password = match hash(&new_user.password, DEFAULT_COST) {
         Ok(h) => h,
         Err(_) => return Err(Status::InternalServerError),
     };
 
-    // Insérer le nouvel utilisateur dans la base de données
     let new_user = NewUser {
         username: new_user.username.clone(),
         email: new_user.email.clone(),
         password: hashed_password,
     };
 
-    let result = diesel::insert_into(users::table)
+    let result = diesel::insert_into(schema::users::table)
         .values(&new_user)
         .execute(&*conn);
 
@@ -86,30 +71,24 @@ pub fn register(new_user: Json<NewUser>, conn: DbConn) -> Result<JsonValue, Stat
 pub fn login(login_user: Json<LoginUser>, conn: DbConn) -> Result<JsonValue, Status> {
     use schema::users::dsl::*;
 
-    // Vérifier si l'email existe
     let user = users
         .filter(email.eq(&login_user.email))
         .first::<User>(&*conn)
         .optional()
-        .expect("Error loading user");
+        .map_err(|_| Status::InternalServerError)?;
 
-    if user.is_none() {
-        return Err(Status::Unauthorized);
-    }
-
-    let user = user.unwrap();
-
-    // Vérifier le mot de passe
-    let password_matches = match verify(&login_user.password, &user.password) {
-        Ok(m) => m,
-        Err(_) => return Err(Status::Unauthorized),
+    let user = match user {
+        Some(user) => user,
+        None => return Err(Status::Unauthorized),
     };
+
+    let password_matches = verify(&login_user.password, &user.password)
+        .map_err(|_| Status::InternalServerError)?;
 
     if !password_matches {
         return Err(Status::Unauthorized);
     }
 
-    // Créer le token JWT
     let expiration = chrono::Utc::now() + chrono::Duration::hours(24);
     let claims = Claim {
         email: user.email.clone(),
@@ -120,13 +99,11 @@ pub fn login(login_user: Json<LoginUser>, conn: DbConn) -> Result<JsonValue, Sta
 
     let header = Header::default();
     let encoding_key = EncodingKey::from_secret(get_jwt().unwrap().as_ref());
-    let token = match encode(&header, &claims, &encoding_key) {
-        Ok(t) => t,
-        Err(_) => return Err(Status::InternalServerError),
-    };
+    let token = encode(&header, &claims, &encoding_key)
+        .map_err(|_| Status::InternalServerError)?;
 
     Ok(json!({
         "message": "User logged in successfully",
-        "token": token
+        "token": token,
     }))
 }
